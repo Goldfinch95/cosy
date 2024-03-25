d0e2cfa7157b64b3454e0774acfbfa93a8260afa
Goldfinch <facu_vila@hotmail.com>
2024-03-25 14:12:38 -0300

    se trabajo en la logica de la pagina de login y register

--- src/app/pages/register/register.component.ts
+++ src/app/pages/register/register.component.ts
@@ -1,12 +1,36 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { RouterLink } from '@angular/router';
+
 
 @Component({
   selector: 'app-register',
   standalone: true,
-  imports: [],
+  imports: [FormsModule, RouterLink],
   templateUrl: './register.component.html',
   styleUrl: './register.component.css'
 })
 export class RegisterComponent {
+  accountData: User;
 
+  constructor(){
+    this.accountData = new User();
+  }
+
+  checkIn(){
+    console.log(this.accountData)
+  }
+}
+
+export class User{
+  email: string;
+  name: string;
+  password: string;
+  secondPassword: string;
+  constructor(){
+    this.email = '';
+    this.name = '';
+    this.password = '';
+    this.secondPassword = '';
+  }
 }
