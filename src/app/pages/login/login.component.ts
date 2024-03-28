b68884a629095457e91902392d0a9b8d598d746d
Goldfinch <facu_vila@hotmail.com>
2024-03-28 07:13:52 -0300

    se trabajo en la parte visual de la pagina login y la logica de validacion de los formularios

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,35 +1,41 @@
 import { Component } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { RouterLink } from '@angular/router';
-
-
+import { ReactiveFormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [FormsModule, RouterLink],
+  imports: [ ReactiveFormsModule,RouterLink, CommonModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
 export class LoginComponent {
+  loginForm!:FormGroup
+  submitted= false;
+  send = false;
+  constructor(private formBuilder:FormBuilder){
+    //this.loginData = new Login();
+  }
+  //loginData: Login;
 
-  loginData: Login;
-
-  constructor(){
-    this.loginData = new Login();
+  ngOnInit(){
+    //validations
+    this.loginForm = this.formBuilder.group({
+      'email': ['', [Validators.required,Validators.email,Validators.pattern(/^.+@.+\..+$/)]],
+      'password': ['',[Validators.required,Validators.minLength(8),Validators.pattern(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]+$/)]]
+    });
   }
 
-  getInto(){
-    console.log(this.loginData)
+  onSubmit(){
+    this.submitted = true;
+    if(this.loginForm.invalid){
+      return
+    }
+    alert('success')
+    console.log(this.loginForm)
   }
-
 }
 
-export class Login{
-  email: string;
-  password: string;
-  constructor(){
-    this.email = '';
-    this.password = '';
-  }
-}
+
