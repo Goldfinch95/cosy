29f2716d8ebd4133f25e75eb1e38df9c6bba0c06
Goldfinch <facu_vila@hotmail.com>
2024-04-09 05:21:59 -0300

    se trabajo en el diseño del componente post-card

--- src/app/components/post-card/post-card.component.ts
+++ src/app/components/post-card/post-card.component.ts
@@ -1,12 +1,18 @@
 import { Component } from '@angular/core';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { faComments, faHeart, faShare } from '@fortawesome/free-solid-svg-icons';
+
+
 
 @Component({
   selector: 'app-post-card',
   standalone: true,
-  imports: [],
+  imports: [FontAwesomeModule],
   templateUrl: './post-card.component.html',
   styleUrl: './post-card.component.css'
 })
 export class PostCardComponent {
-
+  faHeart = faHeart;
+  faComments = faComments;
+  faShare = faShare;
 }
